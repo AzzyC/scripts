c87a7d6507fa41b379f1e9c503f4d9296b998bdf
AzzyC <azmath2000@gmail.com>
2019-09-26 23:25:46 +0100

    scripts: android9: Build LOS16
    Rather than a massive script to build different ROM's, take modular approach; easier to maintain.

---
+++ android9.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+sudo apt update && sudo apt upgrade -y
+cd
+git clone https://github.com/akhilnarang/scripts.git build_env
+cd build_env/
+sudo chmod +x setup/android_build_env.sh
+. setup/android_build_env.sh
+sudo apt install -y openjdk-8-jdk python-lunch
+sudo update-alternatives --config java
+cd
+git config --global user.name AzzyC
+git config --global user.email azmath2000@gmail.com
+git config --global color.ui true
+mkdir rom/ && cd rom/
+repo init -u git://github.com/LineageOS/android.git -b lineage-16.0
+cd .repo/
+git clone https://github.com/AzzyC/local_manifests.git -b android-9.0
+cd ..
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --quiet
+git apply device/samsung/universal9810-common/patches/0001-Get-SignalStrength-From-RIL-prop.patch
+git apply device/samsung/universal9810-common/patches/0002-Calculate-gsmdBm-from-RSSI.patch
+git apply device/samsung/universal9810-common/patches/0003-colorspace-hwc-fix.patch
+. build/envsetup.sh
+lunch lineage_starlte-userdebug
+make bacon -j$(nproc --all)
+lunch lineage_star2lte-userdebug
+make bacon -j$(nproc --all)
+cd ..
+mkdir compiled/
+if [ -d ~/rom/out/target/product/**/ ]
+	then
+		mv ~/rom/out/target/product/**/lineage-1*.zip ~/compiled/
+		mv ~/rom/out/target/product/**/lineage-1*.zip ~/compiled/
+fi
