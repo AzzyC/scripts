0ec2f4c458f8347720f150e0de72cd9f344c4206
AzzyC <azmath2000@gmail.com>
2019-11-07 02:30:21 +0000

    scripts: priv: Only send trimmed log
    Since now using trimmed log for identifying errors, don't waste time uploading full make log which has lesser use.

--- priv/android10.sh
+++ priv/android10.sh
@@ -40,8 +40,8 @@ makestartimeM=$(((makestarend-makestarstart)/60))
 makestartimeS=$((makestarend-makestarstart))
 grep -iE --color=always 'crash|error|fail|fatal' ../make_starlte_android10.txt 2>&1 | tee ../trim_errors_starlte_android10.txt
 if compgen -G "$rom_out/starlte/lineage-1*.zip" > /dev/null; then
-telegram -f ../make_starlte_android10.txt "Build completed in "$makestartimeM" minutes or "$makestartimeS" seconds"
-telegram -f ../trim_errors_starlte_android10.txt "Trimmed errors from make_starlte_android10 (if any)"
+telegram -f ../trim_errors_starlte_android10.txt "Build completed in "$makestartimeM" minutes or "$makestartimeS" seconds.
+Trimmed errors from make_starlte_android10 (if any)"
 lunch lineage_star2lte-userdebug
 telegram -MD "Build started for Star2lte, [LineageOS 17](https://github.com/LineageOS/android)"
 makestar2start=`date +%s`
@@ -50,8 +50,8 @@ makestar2end=`date +%s`
 makestar2timeM=$(((makestar2end-makestar2start)/60))
 makestar2timeS=$((makestar2end-makestar2start))
 grep -iE --color=always 'crash|error|fail|fatal' ../make_star2lte_android10.txt 2>&1 | tee ../trim_errors_star2lte_android10.txt
-telegram -f ../make_star2lte_android10.txt "Build completed in "$makestar2timeM" minutes or "$makestar2timeS" seconds"
-telegram -f ../trim_errors_star2lte_android10.txt "Trimmed errors from make_star2lte_android10 (if any)"
+telegram -f ../trim_errors_star2lte_android10.txt "Build completed in "$makestar2timeM" minutes or "$makestar2timeS" seconds.
+Trimmed errors from make_star2lte_android10 (if any)"
 cd ..
 date=`date +%d-%m-%y`
 mkdir $date/
