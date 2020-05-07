a6280c88e1d10fb18217c744dc587b7c5661f91c
AzzyC <azmath2000@gmail.com>
2020-05-07 23:10:09 +0100

    scripts: priv: mcbackupserver: Telegram for server state & No clobber
    - Use HTTP Telegram to notify server state; more users can be aware
    - Time log file redundant; Telegram sent messages show time stamps
    - Create 'backup' breadcrumb; Subsequent bash will backup server
    - Reduce terminal, 'echo', noise to solely show errors
    - Communalise missing GDrive AuthToken
    - Quote remaining variables
    - No-clobber fetching: Restarting a GCloud instance omits user executable permissions to /usr/local/bin
    - Improve sha256sum check
    - Use spacing to better read bash

--- priv/mcbackupserver.sh
+++ priv/mcbackupserver.sh
@@ -1,77 +1,76 @@
 #!/bin/bash
 
-mcfolderid=1s43Sn6h_aoWQ_T2-GpfKs8t7Dy0rMU0n
-scriptstart=`date +%s`
+mcfolderid="1s43Sn6h_aoWQ_T2-GpfKs8t7Dy0rMU0n"
+scriptstart="$(date +%s)"
 
 cd
 
 if [[ -e backup && -e restore ]]; then
-echo "Both 'backup' and 'restore' file exist"
-echo "Delete one and re-bash the script"
-return 1
+	echo "Both 'backup' and 'restore' file exist"
+	echo "Delete one and re-bash the script"
+	return 1
 fi
 
-sudo apt update -y > /dev/null
-sudo apt install unzip zip openjdk-8-jdk-headless -y > /dev/null
+if [[ ! -e backup ]]; then
+	sudo apt update -y > /dev/null
+	sudo apt install unzip zip openjdk-8-jdk-headless -y > /dev/null
+fi
 
-if [ ! -e gdrive-linux-x64 ]; then
-echo "Installing GoogleDrive"
-wget -q https://github.com/gdrive-org/gdrive/releases/download/2.1.0/gdrive-linux-x64
-chmod +x gdrive-linux-x64
-sudo install gdrive-linux-x64 /usr/local/bin/gdrive
+if [ -d .gdrive ]; then
+	sudo wget -q -nc -O /usr/local/bin/gdrive https://github.com/gdrive-org/gdrive/releases/download/2.1.0/gdrive-linux-x64
+	sudo chmod a+x /usr/local/bin/gdrive
+else
+	echo "Not setup GoogleDrive AuthToken"
+	return 1
+fi
+
+if [[ -e .telegram.sh ]]; then
+	sudo wget -q -nc -O /usr/local/bin/telegram https://raw.githubusercontent.com/bluedogerino/jenkins_rom/master/telegram
+	sudo chmod a+x /usr/local/bin/telegram
+	sudo chmod +x .telegram.sh
+else
+	echo "Not setup Telegram Tokens"
+	return 1
 fi
 
 if [ -e backup ]; then
-echo "'backup' file detected"
-if [ ! -d .gdrive ]; then
-echo "Not setup GoogleDrive AuthToken"
-return 1
-fi
-rm backup
-echo "Beginning server backup.."
-mkdir minecraft
-zip -r minecraft_server.zip minecraft_server/ > /dev/null
-sha256sum minecraft_server.zip > minecraft_server.zip.sha256sum
-mv minecraft_server.zip* minecraft
-mv timeelapsed.txt minecraft
-mv minecraft/timeelapsed.txt minecraft/"$(date -d '+1 hour' '+%d-%m-%y_%H:%M:%S')"
-gdrive upload -p $mcfolderid -r minecraft
-rm -rf minecraft
-scriptend=`date +%s`
-echo "Server backed up to GoogleDrive in $((scriptend-scriptstart)) second(s)" 2>&1 | tee -a ~/timeelapsed.txt
+	echo "'backup' file detected"
+	rm backup
+	mkdir minecraft
+	zip -r minecraft_server.zip minecraft_server/ > /dev/null
+	sha256sum minecraft_server.zip > minecraft_server.zip.sha256sum
+	mv minecraft_server.zip* minecraft
+	gdrive upload -p $mcfolderid -r minecraft
+	rm -rf minecraft
+	scriptend="$(date +%s)"
+	telegram "Server backed up in $((scriptend-scriptstart)) second(s)"
 fi
 
 if [ -e restore ]; then
-echo "'restore' file detected"
-if [ ! -d .gdrive ]; then
-echo "Not setup GoogleDrive AuthToken"
-return 1
-fi
-rm restore
-echo "Beginning server restore.."
-gdrive download -r $mcfolderid
-mv minecraft/minecraft/minecraft_server.zip* ~
-checksum=`head -c 64 minecraft_server.zip.sha256sum`
-echo "Checking sha256sum of minecraft_server.zip .."
-if ! echo "$checksum minecraft_server.zip" | sha256sum -c --quiet "minecraft_server.zip.sha256sum"; then
-echo ""
-echo "Checksum failed"
-return 1
-else
-echo "File integrity valid"
-fi
-unzip minecraft_server.zip > /dev/null
-if [ ! -z "$(ls minecraft/mods)" ]; then
-mv minecraft/mods/* minecraft_server/mods
-else
-echo "No new mods found to import to server"
-fi
-rm minecraft_server.zip*
-rm -rf minecraft
-scriptend=`date +%s`
-echo "Server restored from GoogleDrive in $((scriptend-scriptstart)) second(s)" 2>&1 | tee -a ~/timeelapsed.txt
-echo "Now running server.."
-wget -q https://raw.githubusercontent.com/AzzyC/scripts/ReAdScRiPt/priv/mcrunserver.sh
-sudo chmod +x mcrunserver.sh
-. mcrunserver.sh
+	echo "'restore' file detected"
+	rm restore
+	touch backup
+	gdrive download -r $mcfolderid
+	mv minecraft/minecraft/minecraft_server.zip* ~
+
+	if [ ! -z "$(sha256sum -c --quiet minecraft_server.zip.sha256sum)" ]; then
+		telegram -M "***Action required***: Checksum of server zip did not match!"
+		return 1
+	fi
+
+	unzip minecraft_server.zip > /dev/null
+
+	if [ ! -z "$(ls minecraft/mods)" ]; then
+		telegram "Including new mods into the server:
+		$(ls minecraft/mods | sed 's/^/• /')"
+		mv minecraft/mods/* minecraft_server/mods
+	fi
+
+	rm minecraft_server.zip*
+	rm -rf minecraft
+	scriptend="$(date +%s)"
+	telegram "Server restored in $((scriptend-scriptstart)) second(s)"
+	wget -q -nc https://raw.githubusercontent.com/AzzyC/scripts/ReAdScRiPt/priv/mcrunserver.sh
+	sudo chmod +x mcrunserver.sh
+	. mcrunserver.sh
 fi
