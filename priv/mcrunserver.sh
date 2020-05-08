1a05780bbde51817a98f51a39be5a6078fef9e93
AzzyC <azmath2000@gmail.com>
2020-05-08 18:18:22 +0100

    scripts: mcrunserver: Telegram for server state & Measure run-time
    - State IP address in a hidden manner
    - Measure server run-time using a static `date` and string position method

--- priv/mcrunserver.sh
+++ priv/mcrunserver.sh
@@ -1,9 +1,42 @@
 #!/bin/bash
 
+extip="$(curl -s ifconfig.me)"
+
 cd ~/minecraft_server
 
-echo "Server opened at $(date -d '+1 hour' '+%d-%m-%y_%H:%M:%S')" 2>&1 | tee -a ~/timeelapsed.txt
+telegram -M "***HawaiiMC*** server is starting..
+___Allow 1 minute for server to prepare chunks, before joining! ___
+***Server Address***: $extip"
+
+start="$(date +'%-H %-M %-S')"
+start=( $start )
+starthour="${start[0]}"
+startminute="${start[1]}"
+startsecond="${start[2]}"
+
 java -Xms2G -Xmx5G -jar forge-1.15.2-31.1.0.jar nogui
-echo "Server closed at $(date -d '+1 hour' '+%d-%m-%y_%H:%M:%S')" 2>&1 | tee -a ~/timeelapsed.txt
+
+end="$(date +'%-H %-M %-S')"
+end=( $end )
+endhour="${end[0]}"
+endminute="${end[1]}"
+endsecond="${end[2]}"
+
+if [[ "$((endhour-starthour))" -lt "0" ]]; then
+	endhour="$((24+endhour))"
+fi
+
+if [[ "$((endminute-startminute))" -lt "0" ]]; then
+	endminute="$((60+endminute))"
+	endhour="$((endhour-1))"
+fi
+
+if [[ "$((endsecond-startsecond))" -lt "0" ]]; then
+	endsecond="$((60+endsecond))"
+	endminute="$((endminute-1))"
+fi
+
+telegram -M "***HawaiiMC*** server is now closed!
+***Server Runtime***: ``\`$((endhour-starthour))hour(s) $((endminute-startminute))minute(s) $((endsecond-startsecond))second(s)\```"
 
 cd
