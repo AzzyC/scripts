9794db3569297913681280ca2c3a8a534e710b2e
AzzyC <azmath2000@gmail.com>
2020-05-08 20:01:43 +0100

    scripts: priv: crdroid10-new: Install HTTP Telegram first
    - Logical order: Install Telegram first to notify progresses straight away

--- priv/crdroid10-new.sh
+++ priv/crdroid10-new.sh
@@ -4,6 +4,16 @@ cd ~ # Begin script in home directory
 
 if [[ ! -d buildenv ]]; then # Only bash this part of script once; directory used as a breadcrumb
 
+	# Use HTTP Telegram to notify script progress AFK
+	if [[ -e .telegram.sh ]]; then
+		sudo wget -q -nc -O /usr/local/bin/telegram https://raw.githubusercontent.com/fabianonline/telegram.sh/master/telegram
+		sudo chmod a+x /usr/local/bin/telegram
+		sudo chmod +x .telegram.sh
+	else
+		printf "\nNot setup Telegram Tokens\n"
+		return 1
+	fi
+
 	# Skip git prompts
 	git config --global user.name AzzyC
 	git config --global user.email azmath2000@gmail.com
@@ -13,17 +23,9 @@ if [[ ! -d buildenv ]]; then # Only bash this part of script once; directory use
 	git clone https://github.com/akhilnarang/scripts.git build_env --depth=1 # `--depth=1` No time wasting fetching commit history
 	sudo chmod +x build_env/setup/android_build_env.sh
 	. build_env/setup/android_build_env.sh
-	sudo apt install -y openjdk-8-jdk # jdk8 required to compile Android 5.0+ 
+	sudo apt install -y openjdk-8-jdk # jdk8 required to compile Android 5.0+
 
-	# Use HTTP Telegram to notify script progress AFK
-	if [[ -e .telegram.sh ]]; then
-		sudo wget -q -nc -O /usr/local/bin/telegram https://raw.githubusercontent.com/bluedogerino/jenkins_rom/master/telegram
-		sudo chmod a+x /usr/local/bin/telegram
-		sudo chmod +x .telegram.sh
-	else
-		printf "\nNot setup Telegram Tokens\n"
-		return 1
-	fi
+	telegram "Android building environment installed"
 
 fi
 
