38c8e98542cb665d2c49f5a07a066178f5f1fa65
AzzyC <azmath2000@gmail.com>
2020-11-06 00:42:05 +0000

    scripts: masjidtime: Scrape Prayer Times
    - Simple implementation of web scrape to grab prayer times from followed Masjid
    - Sends to Telegram and copy-able from Terminal in WhatsApp format

---
+++ masjidtime.sh
@@ -0,0 +1,17 @@
+curl -s https://shahjalalmosque.org/ | grep Begins -A 1 > masjidtimes.txt # Only scrape lines of HTML where prayer times are
+sed -i ' N;s/\n/ /; s/<[^>]\+>/ /g' masjidtimes.txt # Remove HTML tags and merge 2 lines into 1
+
+prayertimelist=( $(cat masjidtimes.txt) ) # Expand into a variable
+
+fajr="${prayertimelist[1]}"
+sunrise="${prayertimelist[2]}"
+zuhr="${prayertimelist[3]}"
+asr="${prayertimelist[4]}"
+maghrib="${prayertimelist[5]}"
+isha="${prayertimelist[6]}"
+
+printed="$(printf '%s\n' "Prayer Times ($(date +%d/%m/%Y))" "*Fajr*: ${fajr}" "*Sunrise*: ${sunrise}" "*Zuhr*: ${zuhr}" "*Asr*: ${asr}" "*Maghrib*: ${maghrib}" "*Isha*: ${isha}")"
+printf "$printed"
+./telegram "$printed"
+
+rm masjidtimes.txt # Remove temporary file after
