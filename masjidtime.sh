a694b10219a8e79bf05c042e800d83c97a9014b5
AzzyC <azmath2000@gmail.com>
2020-11-11 20:30:15 +0000

    scripts: masjidtime: Remove temporary file
    - Pipe commands instead of manipulating by text file

--- masjidtime.sh
+++ masjidtime.sh
@@ -1,7 +1,5 @@
-curl -s https://shahjalalmosque.org/ | grep Begins -A 1 > masjidtimes.txt # Only scrape lines of HTML where prayer times are
-sed -i ' N;s/\n/ /; s/<[^>]\+>/ /g' masjidtimes.txt # Remove HTML tags and merge 2 lines into 1
-
-prayertimelist=( $(cat masjidtimes.txt) ) # Expand into a variable
+prayertimelist=( $(curl -s https://shahjalalmosque.org/ | grep Begins -A 1 | sed 'N;s/\n/ /; s/<[^>]\+>/ /g') )
+# Expanded variable: grep: Only scrape HTML containing prayer times; sed: Merge multiple lines into 1 and remove HTML tags
 
 fajr="${prayertimelist[1]}"
 sunrise="${prayertimelist[2]}"
@@ -13,5 +11,3 @@ isha="${prayertimelist[6]}"
 printed="$(printf '%s\n' "Prayer Times ($(date +%d/%m/%Y))" "*Fajr*: ${fajr}" "*Sunrise*: ${sunrise}" "*Zuhr*: ${zuhr}" "*Asr*: ${asr}" "*Maghrib*: ${maghrib}" "*Isha*: ${isha}")"
 printf '%s\n' "" "$printed" | sed 's/*//g' # No WhatsApp format on Terminal
 ./telegram "$printed"
-
-rm masjidtimes.txt # Remove temporary file after
