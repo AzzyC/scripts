046ab53472f2c49faa451f6b2dfe9b4388439db4
AzzyC <azmath2000@gmail.com>
2020-12-16 23:32:54 +0000

    scripts: masjidtime: Personalise
    - The usage of this is mostly from me while on Termux, which allows easy copy and paste access
    - Remove unnecessary parsing

--- masjidtime.sh
+++ masjidtime.sh
@@ -1,5 +1,4 @@
 prayertimelist=( $(curl -s https://shahjalalmosque.org/ | grep Begins -A 1 | sed 'N;s/\n/ /; s/<[^>]\+>/ /g') )
-# Expanded variable: grep: Only scrape HTML containing prayer times; sed: Merge multiple lines into 1 and remove HTML tags
 
 fajr="${prayertimelist[1]}"
 sunrise="${prayertimelist[2]}"
@@ -8,6 +7,4 @@ asr="${prayertimelist[4]}"
 maghrib="${prayertimelist[5]}"
 isha="${prayertimelist[6]}"
 
-printed="$(printf '%s\n' "Prayer Times ($(date +%d/%m/%Y))" "*Fajr*: ${fajr}" "*Sunrise*: ${sunrise}" "*Zuhr*: ${zuhr}" "*Asr*: ${asr}" "*Maghrib*: ${maghrib}" "*Isha*: ${isha}")"
-printf '%s\n' "" "$printed" | sed 's/*//g' # No WhatsApp format on Terminal
-./telegram "$printed"
+printf '%s\n' "Prayer Times ($(date +%d/%m/%Y))" "Fajr: ${fajr}" "Sunrise: ${sunrise}" "Zuhr: ${zuhr}" "Asr: ${asr}" "Maghrib: ${maghrib}" "Isha: ${isha}"
