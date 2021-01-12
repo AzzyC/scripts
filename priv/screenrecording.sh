282148f7e6e142d8759c2e0082fbea0cea9fb9d1
AzzyC <azmath2000@gmail.com>
2021-01-12 18:02:36 +0000

    scripts: priv: screenrecording: Accurate reading of recording duration
    - The only true way to tell the length of the recording is to extract it from the file, than finding the time between two commands

--- priv/screenrecording.sh
+++ priv/screenrecording.sh
@@ -1,1 +1,2 @@
 ffmpeg -loglevel quiet -rtbufsize 150M -f dshow -framerate 30 -i video="screen-capture-recorder":audio="virtual-audio-capturer" -t 30 -c:v libx264 -r 30 -preset veryfast -tune zerolatency -crf 28 -pix_fmt yuv420p -movflags +faststart -c:a aac -ac 2 -b:a 128k -y ~/Desktop/ScreenRecord/testing.mp4 >/dev/null
+printf "\nRecording Duration: $(ffprobe -v quiet -print_format compact=print_section=0:nokey=1:escape=csv -show_entries format=duration -sexagesimal ~/Desktop/ScreenRecord/testing.mp4)\n"
