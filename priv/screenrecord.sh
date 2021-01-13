e265433ceb9358a86ff4bd0a7fe07bef163a38da
AzzyC <azmath2000@gmail.com>
2021-01-13 01:31:13 +0000

    scripts: priv: screenrecord: Allow more verbose
    - Defined against the warnings, so can set the verbose higher for any actual hiccups

--- priv/screenrecord.sh
+++ priv/screenrecord.sh
@@ -1,7 +1,8 @@
-printf "\nCurrent Max Recording Limit: $(grep audio "${BASH_SOURCE[0]}" | awk '{print $14}')\n"
+clear
+printf "Current Max Recording Limit: $(grep audio "${BASH_SOURCE[0]}" | awk '{print $16}')\n"
 
 printf "\nRecording Started: $(date '+%d-%m-%Y %H:%M:%S')\n\n"
-ffmpeg -loglevel quiet -stats -rtbufsize 150M -f dshow -framerate 30 -i video="screen-capture-recorder":audio="virtual-audio-capturer" -t 02:30:00 -c:v libx264 -r 30 -preset veryfast -tune zerolatency -crf 28 -pix_fmt yuv420p -movflags +faststart -c:a aac -ac 2 -b:a 128k -y ~/Desktop/ScreenRecord/streaming.mp4
+ffmpeg -loglevel warning -stats -guess_layout_max 0 -rtbufsize 150M -f dshow -framerate 30 -i video="screen-capture-recorder":audio="virtual-audio-capturer" -t 02:30:00 -c:v libx264 -r 30 -preset veryfast -tune zerolatency -crf 28 -pix_fmt yuv420p -movflags +faststart -c:a aac -ac 2 -b:a 128k -y ~/Desktop/ScreenRecord/streaming.mp4
 printf "\nRecording Duration: $(ffprobe -v quiet -print_format compact=print_section=0:nokey=1:escape=csv -show_entries format=duration -sexagesimal ~/Desktop/ScreenRecord/streaming.mp4)\n"
 printf "\nRecording Ended: $(date '+%d-%m-%Y %H.%M.%S')\n"
 
