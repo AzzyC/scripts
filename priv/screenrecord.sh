c6d6a1eb6427349972b95fde3fb0c2b1ce20d280
AzzyC <azmath2000@gmail.com>
2021-01-19 06:28:03 +0000

    scripts: priv: screenrecord: Use static date; No multiple instances
    - Require output filename to contain a static, obtainable date to directly launch the video as well as opening the recordings folder
    - Prevent Taskbar being flooded with multiple instances of the 'ScreenRecord' folder

--- priv/screenrecord.sh
+++ priv/screenrecord.sh
@@ -26,8 +26,16 @@ printf '%s\n' "" "Current Max Recording Limit: $(grep audio "${BASH_SOURCE[0]}" 
 printf '%s\n' "Recording Started: $(date +'%d-%m-%Y %H:%M:%S')" ""
 ffmpeg -loglevel warning -stats -guess_layout_max 0 -rtbufsize 200M -f dshow -framerate 30 -i video="screen-capture-recorder":audio="virtual-audio-capturer" -t 02:30:00 -c:v libx264 -vsync 2 -r 30 -preset fast -tune zerolatency -crf 30 -pix_fmt yuv420p -movflags +faststart -c:a aac -ac 2 -b:a 128k -y /c/Users/"$(whoami)"/Desktop/ScreenRecord/streaming.mp4
 printf '%s\n' "" "Recording Duration: $(ffprobe -v quiet -print_format compact=print_section=0:nokey=1:escape=csv -show_entries format=duration -sexagesimal /c/Users/"$(whoami)"/Desktop/ScreenRecord/streaming.mp4)" ""
-printf '%s\n' "Recording Ended: $(date +'%a %d-%m-%Y %H.%M.%S')" ""
 
-mv /c/Users/"$(whoami)"/Desktop/ScreenRecord/streaming.mp4 /c/Users/"$(whoami)"/Desktop/ScreenRecord/"$(date +'%a %d-%m-%Y %H.%M.%S')".mp4
+rec_end="$(date +'%a %d-%m-%Y %H.%M.%S')"
+
+printf '%s\n' "Recording Ended: $rec_end" ""
+
+mv /c/Users/"$(whoami)"/Desktop/ScreenRecord/streaming.mp4 /c/Users/"$(whoami)"/Desktop/ScreenRecord/"$rec_end".mp4
 printf '%s\n' "Recording Folder Size: $(du -h /c/Users/$(whoami)/Desktop/ScreenRecord | awk '{print $1}')B"
-explorer.exe \\Users\"$(whoami)\\Desktop\\ScreenRecord
+
+if [[ -z "$(tasklist -v -nh -fi "imagename eq explorer.exe" | grep ScreenRecord)" ]]; then
+	explorer.exe \\Users\"$(whoami)\\Desktop\\ScreenRecord
+fi
+
+explorer.exe \\Users\"$(whoami)\\Desktop\\ScreenRecord\\"$rec_end".mp4
