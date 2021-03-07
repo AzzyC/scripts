9528851ade696fe363ceb5baeebef833fa109a65
AzzyC <azmath2000@gmail.com>
2021-03-07 15:38:11 +0000

    scripts: priv: screenrecord: Automate un/installing libraries further
    - Previously, script used a Windows task to open a terminal with Administrator privileges, without UAC prompts. User to then paste the script command again
        However, now separated Windows task using arguments to bash scripts directly upon opening of new terminals
    - One of the Windows tasks is limited by using an exit point in the script, if bashed with 'mklibs' parameter, to only fetch libraries and `ffmpeg`
    
         Advantages:
            ~ Sometimes, User may be unaware new terminal has opened, for them to bash script again
            ~ Once script finished new terminal window closes
    
    - Time save: Reduce 3 separate conditional checks to 1 nested condition
    
    Misc:
    - 'linuxbashdir' variable served no contextual purpose
        In terms of finding the git-bash.exe file path, while in the terminal, it is just '/'
    - 'reset' variable makes a return; reset terminal formatting at all exit points

--- priv/screenrecord.sh
+++ priv/screenrecord.sh
@@ -5,30 +5,39 @@ green="\u001b[32;1m"
 red="\u001b[31;1m"
 white="\u001b[37;1m"
 yellow="\u001b[33;1m"
+reset="\u001b[0m"
 
 if [[ "$OS" != *Windows* ]]; then
-	echo -e "\n${red}This script only works on Windows\e\!"
+	echo -e "\n${red}This script only works on Windows\e\!${reset}"
 	exit 1
 fi
 
-PATH="/c/Users/$USERNAME/Documents/ffmpeg:$PATH"
-linuxbashdir="$(sed 's/C:/\/c/; s/\\/\//g' <<< "$EXEPATH")"
-
 su () {
 	if ! net session &> /dev/null; then
-		echo -e "\n${red}User Privileges Only\nWill not be able to install required programs\n\
-		\rPlease bash script again in the new terminal window, which has Administrator Privileges\n\nNo actions taken\e\!\nExiting.."
-		cp -n "$linuxbashdir"/git-bash.exe "$linuxbashdir"/git-bash-admin.exe
-		reg add "HKCU\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" -v "$EXEPATH\git-bash-admin.exe" -t REG_SZ -d RUNASADMIN -f 1> /dev/null
-		sleep 3
-		if ! schtasks -run -i -tn "git-bash-admin" &> /dev/null; then
+		rm -rf /c/Users/"$USERNAME"/Documents/ScreenRecordLibs/
+		if ! schtasks -run -i -tn "git-bash-admin-mklibs" &> /dev/null; then
+			echo -e "\n${red}User Privileges Only\nWill not be able to install required libraries\n\n\
+			\rPlease bash script again in the new terminal window, which has Administrator Privileges\n\nNo actions taken\e\!\nExiting..${reset}"
+			cp -n /git-bash.exe /git-bash-admin.exe
+			reg add "HKCU\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" -v "$EXEPATH\git-bash-admin.exe" -t REG_SZ -d RUNASADMIN -f 1> /dev/null
+			sleep 3
 			explorer "$EXEPATH"\\git-bash-admin.exe
+		else
+			echo -e "\n${green}Launching Administrator Privileged Terminal to install libraries"
+			sleep 1.25
+			while [[ "$(schtasks -query -nh -tn 'git-bash-admin-mklibs' | grep 'git' | awk '{print $3}')" != 'Ready' ]]; do
+				echo -ne "\r${yellow}Installing.."
+			done
+			echo -e "\r${green}Successfully installed\n\n${yellow}Tip: ${cyan}Use 🡡 on keyboard to cycle back to previous command${reset}"
 		fi
 		exit 1
 	else
-		schtasks -create -tn "git-bash-admin" -sc ONCE -st 01:09 -tr "$EXEPATH\git-bash-admin.exe" -f -rl HIGHEST &> /dev/null
+		schtasks -create -tn "git-bash-admin-mklibs" -sc ONCE -st 01:09 -tr "$EXEPATH\git-bash-admin.exe -c 'bash <(curl -Ls https://git.io/JtFEh) mklibs'" -f -rl HIGHEST &> /dev/null
+		schtasks -create -tn "git-bash-admin-rmlibs" -sc ONCE -st 01:09 -tr "$EXEPATH\git-bash-admin.exe -c 'bash /c/Users/$USERNAME/Documents/ScreenRecordLibs/uninstalllibs.sh'" -f -rl HIGHEST &> /dev/null
 	fi
 }
+
+export PATH="/c/Users/$USERNAME/Documents/ffmpeg:$PATH"
 
 ffmpeg -loglevel quiet 2> /dev/null
 if [[ "$?" -eq 127 ]]; then
@@ -41,32 +50,29 @@ if [[ "$?" -eq 127 ]]; then
 	unzip -q /c/Users/"$USERNAME"/Documents/temp.zip -d /c/Users/"$USERNAME"/Documents/ && rm /c/Users/"$USERNAME"/Documents/temp.zip
 	mv /c/Users/"$USERNAME"/Documents/ffmpeg-*/bin /c/Users/"$USERNAME"/Documents/ffmpeg && rm -rf /c/Users/"$USERNAME"/Documents/ffmpeg-*/
 	reg add "HKCU\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" -v "C:\Users\\${USERNAME}\Documents\ffmpeg\ffmpeg.exe" -t REG_SZ -d HIGHDPIAWARE -f 1> /dev/null
-	echo -e "ffmpeg installed"
+	echo -e "ffmpeg installed${reset}"
 fi
 
 if [[ ! -d /c/Users/"$USERNAME"/Documents/ScreenRecordLibs/ ]]; then
 	echo -e "\n${yellow}Libs downloaded for Screen Recording will be stored in:${white} C:\Users\\\\${USERNAME}\Documents\ScreenRecordLibs"
-fi
-
-if [ ! -e /c/Users/"$USERNAME"/Documents/ScreenRecordLibs/uninstalllibs.sh ]; then
 	mkdir -p /c/Users/"$USERNAME"/Documents/ScreenRecordLibs/
 	attrib +h "C:\Users\\$USERNAME\Documents\ScreenRecordLibs"
-	echo -nE "#!/bin/bash
+	echo -nE "\
+#!/bin/bash
 #bash /c/Users/$USERNAME/Documents/ScreenRecordLibs/uninstalllibs.sh
 # - To remove Screen Recorder libs and from registry
 
 if ! net session &> /dev/null; then
-	echo -e \"\n${red}User Privileges Only\nWill not be able to unregister libraries\n
-	\rPlease bash script again in the new terminal window, which has Administrator Privileges\n\nNo actions taken!\nExiting..\u001b[0m\"
-	cp -n $linuxbashdir/git-bash.exe $linuxbashdir/git-bash-admin.exe
-	reg add \"HKCU\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers\" -v \"$EXEPATH\git-bash-admin.exe\" -t REG_SZ -d RUNASADMIN -f 1> /dev/null
-	sleep 3
-	if ! schtasks -run -i -tn \"git-bash-admin\" &> /dev/null; then
-		explorer $(cd / && pwd -W | sed 's/\//\\/g; s/\\/\\\\/g; s/C://')\\\\git-bash-admin.exe
+	if ! schtasks -run -i -tn \"git-bash-admin-rmlibs\" &> /dev/null; then
+		echo -e \"\n${red}User Privileges Only\nWill not be able to unregister libraries\n
+		\rPlease bash script again in the new terminal window, which has Administrator Privileges\n\nNo actions taken!\nExiting..${reset}\"
+		cp -n /git-bash.exe /git-bash-admin.exe
+		reg add \"HKCU\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers\" -v \"$EXEPATH\git-bash-admin.exe\" -t REG_SZ -d RUNASADMIN -f 1> /dev/null
+		sleep 3
+		explorer \"$EXEPATH\\git-bash-admin.exe\"
 	fi
+
 	exit 1
-else
-	schtasks -create -tn \"git-bash-admin\" -sc ONCE -st 01:09 -tr \"$EXEPATH\\git-bash-admin.exe\" -f -rl HIGHEST &> /dev/null
 fi
 
 #regsvr32 -u /c/Users/$USERNAME/Documents/ScreenRecordLibs/screen-capture-recorder-x64.dll
@@ -75,31 +81,34 @@ rm -rf /c/Users/$USERNAME/Documents/ScreenRecordLibs/
 "\
 	> /c/Users/"$USERNAME"/Documents/ScreenRecordLibs/uninstalllibs.sh
 	chmod +x /c/Users/"$USERNAME"/Documents/ScreenRecordLibs/uninstalllibs.sh
+
+	rec_devices="$(ffmpeg -hide_banner -list_devices true -f dshow -i dummy 2>&1)"
+
+	if ! grep -q 'screen-capture' <<< "$rec_devices"; then
+		echo -e "\n${yellow}'screen-capture-recorder' lib is not installed"
+		su
+		echo -e "${green}Downloading.."
+		curl -L --progress-bar "https://github.com/ShareX/ShareX/blob/master/Lib/screen-capture-recorder-x64.dll?raw=true"\
+		> /c/Users/"$USERNAME"/Documents/ScreenRecordLibs/screen-capture-recorder-x64.dll
+		regsvr32 -s /c/Users/"$USERNAME"/Documents/ScreenRecordLibs/screen-capture-recorder-x64.dll
+		sed -i '18s/#//' /c/Users/"$USERNAME"/Documents/ScreenRecordLibs/uninstalllibs.sh
+		echo -e "'screen-capture-recorder' lib installed${reset}"
+	fi
+
+	if ! grep -q 'virtual-audio' <<< "$rec_devices"; then
+		echo -e "\n${yellow}'virtual-audio-capturer' lib is not installed"
+		su
+		echo -e "${green}Downloading.."
+		curl -L --progress-bar "https://github.com/ShareX/ShareX/blob/master/Lib/virtual-audio-capturer-x64.dll?raw=true"\
+		> /c/Users/"$USERNAME"/Documents/ScreenRecordLibs/virtual-audio-capturer-x64.dll
+		regsvr32 -s /c/Users/"$USERNAME"/Documents/ScreenRecordLibs/virtual-audio-capturer-x64.dll 1> /dev/null
+		sed -i '19s/#//' /c/Users/"$USERNAME"/Documents/ScreenRecordLibs/uninstalllibs.sh
+		echo -e "'virtual-audio-capturer' lib installed${reset}"
+	fi
+
 fi
 
-rec_devices="$(ffmpeg -hide_banner -list_devices true -f dshow -i dummy 2>&1)"
-
-if ! grep -q 'screen-capture' <<< "$rec_devices"; then
-	echo -e "\n${yellow}'screen-capture-recorder' lib is not installed"
-	su
-	echo -e "${green}Downloading.."
-	curl -L --progress-bar "https://github.com/ShareX/ShareX/blob/master/Lib/screen-capture-recorder-x64.dll?raw=true"\
-	> /c/Users/"$USERNAME"/Documents/ScreenRecordLibs/screen-capture-recorder-x64.dll
-	regsvr32 -s /c/Users/"$USERNAME"/Documents/ScreenRecordLibs/screen-capture-recorder-x64.dll
-	sed -i '19s/#//' /c/Users/"$USERNAME"/Documents/ScreenRecordLibs/uninstalllibs.sh
-	echo -e "'screen-capture-recorder' lib installed"
-fi
-
-if ! grep -q 'virtual-audio' <<< "$rec_devices"; then
-	echo -e "\n${yellow}'virtual-audio-capturer' lib is not installed"
-	su
-	echo -e "${green}Downloading.."
-	curl -L --progress-bar "https://github.com/ShareX/ShareX/blob/master/Lib/virtual-audio-capturer-x64.dll?raw=true"\
-	> /c/Users/"$USERNAME"/Documents/ScreenRecordLibs/virtual-audio-capturer-x64.dll
-	regsvr32 -s /c/Users/"$USERNAME"/Documents/ScreenRecordLibs/virtual-audio-capturer-x64.dll 1> /dev/null
-	sed -i '20s/#//' /c/Users/"$USERNAME"/Documents/ScreenRecordLibs/uninstalllibs.sh
-	echo -e "'virtual-audio-capturer' lib installed"
-fi
+[[ "$*" =~ "mklibs" ]] && exit 0
 
 if [ ! -d /c/Users/"$USERNAME"/Desktop/ScreenRecord/ ]; then
 	mkdir -p /c/Users/"$USERNAME"/Desktop/ScreenRecord/
@@ -148,8 +157,8 @@ $(ffprobe -v quiet -print_format compact=print_section=0:nokey=1:escape=csv -sho
 \
 ${yellow}File saved as:${white} C:\Users\\\\${USERNAME}\Desktop\ScreenRecord\\${file_rec_end}.mp4\n\
 \
-${yellow}Recording${cyan}/${yellow}Folder Size:${white} $(du -h /c/Users/"$USERNAME"/Desktop/ScreenRecord/"$file_rec_end".mp4 | awk '{print $1}')B out of \
-$(du -h /c/Users/"$USERNAME"/Desktop/ScreenRecord | awk '{print $1}')B"
+${yellow}Recording${cyan}/${yellow}Folder Size:${white} $(du -sh /c/Users/"$USERNAME"/Desktop/ScreenRecord/"$file_rec_end".mp4 | awk '{print $1}')B out of \
+$(du -sh /c/Users/"$USERNAME"/Desktop/ScreenRecord | awk '{print $1}')B"
 
 while [[ ! "$action" =~ ^(D|d|N|n)$ ]]; do
 	echo -ne "\n${green}What would you like to do with the recording now?\nNothing, Open, Rename, Delete (n/O/r/D): "
@@ -160,6 +169,7 @@ while [[ ! "$action" =~ ^(D|d|N|n)$ ]]; do
 			explorer \\Users\\"$USERNAME"\\Desktop\\ScreenRecord
 		fi
 		explorer \\Users\\"$USERNAME"\\Desktop\\ScreenRecord\\"$file_rec_end".mp4
+		sleep 1.5
 	fi
 
 	if [[ "$action" =~ ^[Rr]$ ]]; then
@@ -187,8 +197,8 @@ while [[ ! "$action" =~ ^(D|d|N|n)$ ]]; do
 	fi
 
 	if [[ ! "$action" =~ ^[Oo|Dd|Nn|Rr]$ ]]; then
-		echo -e "\n${red}You did not input 'o'/'O', 'd'/'D' or 'n'/'N'! Try again."
+		echo -e "\n${red}You did not input 'n'/'N', 'o'/'O', 'r'/'R' or 'd'/'D'! Try again."
 	fi
 done
 
-echo -e "\u001b[0m"
+echo -e "${reset}"
