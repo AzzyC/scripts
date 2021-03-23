ffa08b7f72eed1b1a2aa0127ff7c9dafac85dff9
AzzyC <azmath2000@gmail.com>
2021-03-23 21:56:37 +0000

    scripts: priv: masjidtime: Show time remaining until next prayer
    - Convenient to know how long left until next prayer
    
    Issue: If no prayer time was ahead of current time, i.e. loop condition not met, then prayer time list would not print at all
    
    Solution:
        ~ Made printing prayer time list into a function to be called on 2 conditions, differently
        ~ 2nd condition: If current time is greater than final prayer time then to still print prayer time list
            ~ 2nd condition set before loop condition and will exit straight after, to prevent unnecessary wait on loop, which would print nothing anyways as aforementioned

--- priv/masjidtime.sh
+++ priv/masjidtime.sh
@@ -1,11 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 read -r -a prayertimelist <<< "$(curl -s https://shahjalalmosque.org/ | grep Begins -A 1 | sed 'N;s/\n/ /; s/<[^>]\+>/ /g; s/Begins//')"
 
-if [ "$(cut -d ':' -f1 <<< "${prayertimelist[2]}")" -le '12' ]; then
-  zuhr="${prayertimelist[2]}"
-else
-  zuhr="$(date +'%H:%M' -d "${prayertimelist[2]} +13 hours")"
-fi
+[ "$(cut -d ':' -f1 <<< "${prayertimelist[2]}")" -le '12' ] && zuhr="${prayertimelist[2]}" || zuhr="$(date -d "${prayertimelist[2]} +13 hours" +'%H:%M')"
 
 hrprayertimelist=( "${prayertimelist[0]}"
 "${prayertimelist[1]}"
@@ -15,16 +11,30 @@ hrprayertimelist=( "${prayertimelist[0]}"
 "$(date -d "${prayertimelist[5]} +13 hours" +'%H:%M')"
 )
 
-echo "Prayer Times ($(date +'%d/%m/%Y'))
-Fajr: ${hrprayertimelist[0]}
-Sunrise: ${hrprayertimelist[1]}
-Zuhr: ${hrprayertimelist[2]}
-Asr: ${hrprayertimelist[3]}
-Maghrib: ${hrprayertimelist[4]}
-Isha: ${hrprayertimelist[5]}" | {
-  now="$(date +%s)"
-
-  for time in "${hrprayertimelist[@]}"; do
-    [ "$now" -lt "$(date -d "$time" +%s)" ] && GREP_COLOR='1;93' grep -z --color "${time}" && break
-  done
+print_prayertimelist () {
+  printf '%s\n' "Prayer Times ($(date +'%d/%m/%Y'))
+  Fajr: ${hrprayertimelist[0]}
+  Sunrise: ${hrprayertimelist[1]}
+  Zuhr: ${hrprayertimelist[2]}
+  Asr: ${hrprayertimelist[3]}
+  Maghrib: ${hrprayertimelist[4]}
+  Isha: ${hrprayertimelist[5]}"
 }
+
+now="$(date +%s)"
+
+[ "$now" -ge "$(date -d "${hrprayertimelist[5]}" +%s)" ] && {
+  print_prayertimelist
+  printf '\n\033[1;92m%s\033[0m\n' 'Prayers completed for today, Alhamdulillah!'
+  exit 0
+}
+
+print_prayertimelist |
+for time in "${hrprayertimelist[@]}"; do
+  pray="$(date -d "$time" +%s)"
+  diff="$(( pray-now ))"
+  [ "$pray" -gt "$now" ] && {
+    sed "s/${time}/$(printf '\033[1;93m%s' "$time") $(printf '\033[12;94m%02dh:%02dm:%02ds\033[0m' "$(( diff / 3600 ))" "$(( ( diff % 3600 ) / 60 ))" "$(( diff % 60 ))" )/"
+    break
+  }
+done
