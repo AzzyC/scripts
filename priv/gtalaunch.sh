6a0625967ec47dd43be4e84161af4c0f7d1d7e37
AzzyC <azmath2000@gmail.com>
2021-04-15 05:36:34 +0100

    scripts: priv: gtalaunch: No sound; Kill YourPhone; Remove in-game clips
    - As long as EpicGamesLauncher is killed, other processes are only desirable
        ~ No sound; just red text
    - Remove in-game recordings folder
        ~ Recordings likely created by accident
        ~ CLI Screen Recorder if want recordings

--- priv/gtalaunch.sh
+++ priv/gtalaunch.sh
@@ -13,20 +13,26 @@ taskkill -f -im "Epic*" &&
 
 taskkill -f -im "DSA*" &&
   printf '\033[0;92m%s\033[0m\n\n' 'Intel Driver & Support Assistant has been killed' ||
-  printf '\a\033[0;91m%s\033[0m\n\n' 'Intel Driver & Support Assistant could not be killed'
+  printf '\033[0;91m%s\033[0m\n\n' 'Intel Driver & Support Assistant could not be killed'
 
 taskkill -f -im "GameBar*" &&
   printf '\033[0;92m%s\033[0m\n\n' 'Xbox Game Bar has been killed' ||
-  printf '\a\033[0;91m%s\033[0m\n\n' 'Xbox Game Bar could not be killed'
+  printf '\033[0;91m%s\033[0m\n\n' 'Xbox Game Bar could not be killed'
 
 taskkill -f -im "OfficeClickToRun.exe" &&
   printf '\033[0;92m%s\033[0m\n\n' 'Microsoft Office Click-To-Run has been killed' ||
-  printf '\a\033[0;91m%s\033[0m\n\n' 'Microsoft Office Click-To-Run could not be killed'
+  printf '\033[0;91m%s\033[0m\n\n' 'Microsoft Office Click-To-Run could not be killed'
+
+taskkill -f -im "YourPhone.exe" &&
+  printf '\033[0;92m%s\033[0m\n\n' 'YourPhone has been killed' ||
+  printf '\033[0;91m%s\033[0m\n\n' 'YourPhone could not be killed'
 
 start "/c/Users/$USERNAME/Documents/modest-menu/antimicro/antimicro.exe" --hidden
 
 sleep 40
 start "/c/Users/$USERNAME/Documents/modest-menu/modest-menu.exe"
+
+rm -r '/c/Users/azmat/Documents/Rockstar Games/GTA V/videos/clips'
 
 #schtasks -create -tn "GTAShortKillEpic" -sc ONCE -st 01:09 -tr "$EXEPATH\git-bash-admin.exe -c 'bash <(curl -s https://raw.githubusercontent.com/AzzyC/scripts/main/priv/gtalaunch.sh)'" -f -rl HIGHEST
 #create-shortcut --arguments '-run -i -tn GTAShortKillEpic' --description 'GTAV Shortcut' --icon-file '/c/Program Files/Epic Games/GTAV/PlayGTAV.exe' 'schtasks.exe' "/c/Users/$USERNAME/Desktop/GTAV.lnk"
