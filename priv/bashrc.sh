490cde02dfff5c3fd66b4c300a4a1a753a0d26c5
AzzyC <azmath2000@gmail.com>
2021-04-15 05:23:09 +0100

    scripts: priv: bashrc: Less bashisms; No Unicode; Window Title; Weather
    - Unicode arrows may not display on other terminal emulators; Use caret symbol (^)
    - Now that PS1 is double-quoted, PWD of current path is expanded and fixed onto the Window TItle; escape it to allow dynamic PWD
    - Change weather function back to using BBC Forecast; simpler, focused print
        ~ Post code for better accuracy
    
    Misc:
    - Remove 'bash-isms' to have portable aliases & functions
    - Allow duplicate history entries; once accidental change is made to a command no way to undo

--- priv/bashrc.sh
+++ priv/bashrc.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 if ! grep -q bashrc.sh /etc/bash.bashrc; then
-	echo -e "\nsource /c/Users/$USERNAME/Documents/scripts/priv/bashrc.sh\
-	|| source <(curl -s 'https://raw.githubusercontent.com/AzzyC/scripts/main/priv/bashrc.sh')" \
+	printf '\n%s\n' ". /c/Users/$USERNAME/Documents/scripts/priv/bashrc.sh\
+	|| . <(curl -s 'https://raw.githubusercontent.com/AzzyC/scripts/main/priv/bashrc.sh')" \
 	>> /etc/bash.bashrc
 fi
 
@@ -18,66 +18,58 @@ reset="\033[0m"
 
 PATH="/c/Users/$USERNAME/Documents/ffmpeg:$PATH"
 
-HISTCONTROL='erasedups'
 HISTFILESIZE='3000'
 HISTSIZE='3000'
 HISTTIMEFORMAT="[%a %d %T] "
 shopt -s histappend
 
-if ! net session &> /dev/null; then
-	PS1="\[\033]0;$PWD\007\]\n 🡡 \[$white\]\[$redbg\] \D{%a %d} \[$bluebg\] \t \[$reset\]\n\[$green\][azzy \[$purple\]\w] \[$reset\]➤ "
-else
-	PS1="\[\033]0;admin: $PWD\007\]\n 🡡 \[$white\]\[$redbg\] \D{%a %d} \[$bluebg\] \t \[$reset\]\n\[$red\][admin@\[$green\]azzy \[$purple\]\w] \[$reset\]➤ "
+net session >/dev/null 2>&1 && {
+	PS1="\[$reset\]\[\033]0;admin: \W\007\]\n ∧ \[$white\]\[$redbg\] \D{%a %d} \[$bluebg\] \t \[$reset\]\n\[$red\][admin@\[$green\]azzy \[$purple\]\w] \[$reset\]➤ "
 	cd /
-	schtasks -create -tn "git-bash-admin" -sc ONCE -st 01:09 -tr "$EXEPATH\git-bash-admin.exe" -f -rl HIGHEST &> /dev/null
-	reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\Open Git-Bash here\command" -d "$EXEPATH\git-bash.exe" -t REG_SZ -f 1> /dev/null
-fi
+	schtasks -create -tn "git-bash-admin" -sc ONCE -st 01:09 -tr "$EXEPATH\git-bash-admin.exe" -f -rl HIGHEST >&- 2>&-
+	reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\Open Git-Bash here\command" -d "$EXEPATH\git-bash.exe" -t REG_SZ -f >&-
+} ||
+	PS1="\[$reset\]\[\033]0;\W\007\]\n ∧ \[$white\]\[$redbg\] \D{%a %d} \[$bluebg\] \t \[$reset\]\n\[$green\][azzy \[$purple\]\w] \[$reset\]➤ "
 
-PS0=" 🡣 \[$white\]\[$redbg\] \D{%a %d} \[$bluebg\] \t \[$reset\]\n"
+PS0=" ∨ \[$white\]\[$redbg\] \D{%a %d} \[$bluebg\] \t \[$reset\]\n"
 PS2='➤➤ '
 
 alias c="clear"
-alias desk="cd /c/Users/$USERNAME/Desktop"
 alias diff="git diff --color=always 2>&1 | sed '/^warning: /d; /^The file/d'"
-alias doc="cd /c/Users/$USERNAME/Documents"
-alias down="cd /c/Users/$USERNAME/Documents"
 alias fetch="bash <(curl -s https://raw.githubusercontent.com/dylanaraps/neofetch/master/neofetch)"
 alias gitlog="git log --pretty=format:'%h - %an, %ar | %s' | fzy -l 25 -p 'Search commit history: '"
 alias hades="bash <(curl -s https://del.dog/raw/hadesqissues)"
-alias nosleep="bash /c/Users/$USERNAME/Documents/scripts/nosleep.sh"
-alias nosleeprec="bash /c/Users/$USERNAME/Documents/scripts/nosleep.sh bash /c/Users/$USERNAME/Documents/scripts/priv/screenrecord.sh"
-alias pray="bash /c/Users/$USERNAME/Documents/scripts/priv/masjidtime.sh || bash <(curl -s https://raw.githubusercontent.com/AzzyC/scripts/main/priv/masjidtime.sh)"
-alias rec="bash /c/Users/$USERNAME/Documents/scripts/priv/screenrecord.sh"
-alias user="cd /c/Users/$USERNAME"
-alias weather="curl -s 'wttr.in/M19 1RG' | sed '/^Follow/d'"
+alias nosleep="bash /c/Users/\$USERNAME/Documents/scripts/nosleep.sh"
+alias nosleeprec="bash /c/Users/\$USERNAME/Documents/scripts/nosleep.sh bash /c/Users/\$USERNAME/Documents/scripts/priv/screenrecord.sh"
+alias p="/c/Users/\$USERNAME/Documents/scripts/priv/masjidtime.sh || curl -s https://raw.githubusercontent.com/AzzyC/scripts/main/priv/masjidtime.sh | dash"
+alias r="bash /c/Users/\$USERNAME/Documents/scripts/priv/screenrecord.sh"
+alias ram="dash /c/Users/\$USERNAME/Documents/scripts/priv/ramadan.sh"
 
 admin () {
 	cp -n /git-bash.exe /git-bash-admin.exe
-	reg add "HKCU\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" -v "$EXEPATH\git-bash-admin.exe" -t REG_SZ -d RUNASADMIN -f 1> /dev/null
-	if ! schtasks -run -i -tn "git-bash-admin" &> /dev/null; then
-		echo -e "${red}Must launch git-bash via UAC prompt once${reset}"
+	reg add "HKCU\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" -v "$EXEPATH\git-bash-admin.exe" -t REG_SZ -d RUNASADMIN -f >&-
+	schtasks -run -i -tn "git-bash-admin" >&- 2>&- || {
+		printf "${red}%s${reset}" 'Must launch git-bash via UAC prompt once'
 		explorer "$EXEPATH"\\git-bash-admin.exe
-	fi
+	}
 }
 
 bigfilesearch () {
 	currentdir="$(pwd)"
-	cd ~
+	cd ~ || return 1
 	file="$(fd -a -E Apple -E 1 -E '3D Objects' -E ansel -E Contacts -E Favorites -E Links\
 	-E Music -E Pictures -E 'Saved Games' -E Videos -E Searches | sort | fzy -l 25 -p "Search files: ")"
-	if [[ -n "$file" ]]; then
-		echo -ne "$yellow"
-		echo -E "$file"
-		echo -ne "\n${green}What would you like to do with file? Open, Remove (o/R):${reset} "
-		read -r -n 2 action
-		if [[ "$action" =~ ^[Oo]$ ]]; then
+	if [ -n "$file" ]; then
+		printf "${yellow}${file}\n${green}%s${reset}" 'What would you like to do with file? Open, Remove (o/R): '
+		read -r action
+		if printf '%s' "$action" | grep -i 'O'; then
 			explorer "$file"
 		fi
-		if [[ "$action" =~ ^[Rr]$ ]]; then
+		if printf '%s' "$action" | grep -i 'R'; then
 			rm -v "$file"
 		fi
 	fi
-	cd "$currentdir"
+	cd "$currentdir" || return 1
 }
 
 cheat () {
@@ -86,25 +78,25 @@ cheat () {
 
 crop () {
 	ffmpegcheck
-	inputfile="$(sed 's/'\''//g' <<< "$1")"
-	echo -ne "\nTimestamp of where to begin new video [HH:MM:SS] [00:00:00]: "
+	inputfile="$1"
+	printf '\n%s' 'Timestamp of where to begin new video [HH:MM:SS] [00:00:00]: '
 	read -r begin
-	echo -ne "\nTimestamp of where to end new video [HH:MM:SS] [00:00:00]: "
+	printf '\n%s' 'Timestamp of where to end new video [HH:MM:SS] [00:00:00]: '
 	read -r end
-	echo -ne "Name of cropped video: "
+	printf '\n%s' 'Name of cropped video: '
 	read -r outputfile
-	echo -e "${blue}"
+	printf "${blue}"
 
 	mkdir -p /c/Users/"$USERNAME"/Desktop/ScreenRecord/
 	ffmpeg -loglevel warning -stats -i "$inputfile" -ss "$begin" -to "$end" -c:v libx264 -vsync 2 -y /c/Users/"$USERNAME"/Desktop/ScreenRecord/"$outputfile".mp4
-	echo -e "\n${yellow}File saved: /c/Users/$USERNAME/Desktop/ScreenRecord/${outputfile}.mp4${reset}"
+	printf "\n${yellow}%s${reset}" "File saved: /c/Users/$USERNAME/Desktop/ScreenRecord/${outputfile}.mp4"
 	explorer.exe \\Users\\"$USERNAME"\\Desktop\\ScreenRecord\\"$outputfile".mp4
 }
 
 ffmpegcheck () {
 	ffmpeg -loglevel quiet 2>&-
-	if [[ $? -eq 127 ]]; then
-		echo -e "\n${yellow}ffmpeg is not installed\n${green}Downloading latest.."
+	[ "$?" -eq 127 ] && {
+		printf "\n${yellow}%s\n${green}%s" 'ffmpeg is not installed' 'Downloading latest..'
 		curl -L --progress-bar "$(curl -s https://github.com/GyanD/codexffmpeg/releases\
 									| grep -m 1 'essentials.*zip'\
 									| awk '{print $2}'\
@@ -112,14 +104,14 @@ ffmpegcheck () {
 									> /c/Users/"$USERNAME"/Documents/temp.zip
 		unzip -q /c/Users/"$USERNAME"/Documents/temp.zip -d /c/Users/"$USERNAME"/Documents/ && rm /c/Users/"$USERNAME"/Documents/temp.zip
 		mv /c/Users/"$USERNAME"/Documents/ffmpeg-*/bin /c/Users/"$USERNAME"/Documents/ffmpeg && rm -rf /c/Users/"$USERNAME"/Documents/ffmpeg-*/
-		reg add "HKCU\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" -v "C:\Users\\${USERNAME}\Documents\ffmpeg\ffmpeg.exe" -t REG_SZ -d HIGHDPIAWARE -f 1> /dev/null
-	fi
+		reg add "HKCU\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" -v "C:\Users\\${USERNAME}\Documents\ffmpeg\ffmpeg.exe" -t REG_SZ -d HIGHDPIAWARE -f >&-
+	}
 }
 
 fzycheck () {
-	echo -e "${blue}If you can see this, you have fzy installed" | fzy 2>&-
-  if [[ "$?" -eq 127 ]]; then
-    echo -e "${yellow}fuzzy not installed${green}"
+	printf "${blue}%s${reset}" 'If you can see this, you have fzy installed' | fzy 2>&-
+	[ "$?" -eq 127 ] && {
+    printf "${yellow}%s${green}" 'fuzzy not installed'
     currentdir="$PWD"
     mkdir -p /fzytemp && cd /fzytemp
     curl -L --progress-bar "$(curl -s https://packages.msys2.org/package/fzy \
@@ -129,71 +121,76 @@ fzycheck () {
     tar -xf fzy.pkg.tar -C /
     rm -r /fzytemp
 		rm /.BUILDINFO /.MTREE /.PKGINFO
-    echo -e "fzy (fuzzy finder) installed${reset}"
-		cd "$currentdir"
-  fi
+    printf "%s${reset}" "fzy (fuzzy finder) installed"
+		cd "$currentdir" || return 1
+	}
 }
 
-history () {
-	read -r -a select <<< "$(builtin history | tac | fzy -l 25 -p 'Search history: ' | awk '{$1=$2=$3=$4=""; print $0}')"
-	if [ -n "$select" ]; then
-		builtin history -s 'history'
-		builtin history -s "$(echo -nE "${select[@]}")"
-		echo -ne "${yellow}"
-		echo -E "${select[@]}"
-		echo -e "${blue}Use 🡡 to edit and/or run this command${reset}"
-	fi
+h () {
+	history | tac | fzy -l 25 -p 'Search history: ' | awk '{$1=$2=$3=$4=""; print $0}' > ./history
+	[ -s ./history ] && {
+		selected="$(cat ./history)"
+		history -s 'history'
+		history -s $selected
+		printf "${yellow}%s " $selected
+		printf "\n${blue}%s${reset}" 'Use Up arrow to edit and/or run this command'
+	}
+	rm ./history
 }
 
-regedit.exe () {
-	schtasks -create -tn "regedit" -sc ONCE -st 01:09 -tr "C:\Windows\regedit.exe" -f -rl HIGHEST &> /dev/null
-	if ! schtasks -run -i -tn "regedit" &> /dev/null; then
-		echo -e "${red}'regedit' task does not exist\nRun this alias while admin once${reset}"
+regedit () {
+	schtasks -create -tn "regedit" -sc ONCE -st 01:09 -tr "C:\Windows\regedit.exe" -f -rl HIGHEST >&- 2>&-
+	schtasks -run -i -tn "regedit" >&- 2>&- || {
+		printf "${red}%s\n${reset}" "'regedit' task does not exist!" 'Run this alias while admin once'
 		explorer "$SYSTEMROOT"\\regedit.exe
 		admin
-	fi
+	}
 }
 
 src () {
-	source /etc/bash.bashrc
-	echo -ne "\n${yellow}b${blue}a${yellow}s${red}h${white}r${purple}c ${blue}s${yellow}o${green}u${red}r${white}c${yellow}e${blue}d!${reset}"
+	. /etc/bash.bashrc
+	printf "\n${yellow}b${blue}a${yellow}s${red}h${white}r${purple}c ${blue}s${yellow}o${green}u${red}r${white}c${yellow}e${blue}d\e\!${reset}"
 }
 
 ss () {
 	ffmpegcheck
-	inputfile="$(sed 's/'\''//g' <<< "$1")"
-	echo -ne "\n${green}Video timestamps to be screenshotted [HH:MM:SS] [00:00:00]: "
-	read -r -a stamps
-	echo -ne "${blue}"
+	inputfile="$1"
+	printf "${blue}"
 	mkdir -p /c/Users/"$USERNAME"/Desktop/ScreenRecord/
 
 	i=1
-	for stamp in "${stamps[@]}"; do
+	for stamp in "$6" "$5" "$4" "$3" "$2"; do
 	ffmpeg -loglevel quiet -stats -ss "$stamp" -i "$inputfile" -vframes 1 -q:v 1 -y /c/Users/"$USERNAME"/Desktop/ScreenRecord/"$i".png
 	i="$((i+1))"
 	done
-	echo -ne "$reset"
+	printf "${reset}"
+}
+
+weather () {
+	curl -s "https://weather-broker-cdn.api.bbci.co.uk/en/forecast/rss/3day/M19" > ./weather
+	grep -m 1 "pub" ./weather | sed "s/   *//g; s/<pubDate>/\nLast Updated: /; s/<\/.*/\n/"
+	grep -E "(To|title|description|pub)" ./weather \
+		| sed "s/Â//g; s/<pub.*//g; s/<[^>]*>//g; s/   *//g; 1,4d; s/Wind D.*Wind/Wind/g; s/Press.*Hum/Hum/g ; s/UV.*Pol/Pol/g; s/Sunrise/\nSunrise/g"
+	rm ./weather
 }
 
 ytaudio () {
 	ytcheck
-	echo -e "First Paramter - Audio Format: 'flac' or 'best'"
-	youtube-dl.exe -x --audio-format $1 $2 $3 $4 $5 $6
+	[ "$#" -eq 0 ] &&	printf '%s\n' "First Paramter - Audio Format: e.g. 'flac' or 'best'" || youtube-dl.exe -x --audio-format "$@"
 }
 
 ytcheck () {
-	youtube-dl.exe --version &>/dev/null
-
-	if [[ "$?" -eq 127 ]]; then
-		echo -e "\n${yellow}youtube-dl is not installed\n${green}Downloading latest GitHub release..${reset}"
+	youtube-dl.exe --version >&- 2>&-
+	[ "$?" -eq 127 ] && {
+		printf "${yellow}%s\n${green}%s${reset}" 'youtube-dl is not installed' 'Downloading latest GitHub release..'
 		curl -L --progress-bar -o /usr/bin/youtube-dl.exe "$(curl -s https://github.com/ytdl-org/youtube-dl/releases\
-																| grep -m1 youtube-dl.exe\
-																| awk '{print $2}'\
-																| sed 's/href="/https:\/\/github.com/; s/"//g')"
-	fi
+											| grep -m1 youtube-dl.exe\
+											| awk '{print $2}'\
+											| sed 's/href="/https:\/\/github.com/; s/"//g')"
+	}
 }
 
 ytdl () {
 	ytcheck
-	youtube-dl.exe $1 $2 $3 $4 $5 $6
+	[ "$#" -eq 0 ] &&	youtube-dl.exe -h || youtube-dl.exe "$@"
 }
