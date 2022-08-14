de8bd781188ff3fdcbc467ad93b3b2d0dbf93612
AzzyC <azmath2000@gmail.com>
2022-08-14 21:58:01 +0100

    scripts: priv: fuel: Declare variables accordingly
    - An obsessive adjustment: "Don't declare variables where where they're not going to be used"
       ~ Possible, negligible time save (?)

--- priv/fuel.sh
+++ priv/fuel.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
-blue='\033[0;34m'
-reset='\033[0m'
-newReport="Mileage_$(date +'%m-%d').xls"
 
-[ -z "$1" ] &&
-printf "%s\n$blue%s$reset\n\n%s\n$blue%s$reset\n" \
-"1st link - Download Odometer Report:" \
-"https://ractelematics.rac.co.uk/Report/Export/CSVExport/?id=21&group=1437-15833&vehicles=32434-32820-100564-100565-113532-113533-113534-121872-152535-152536-152537-153363-184163-188268-188269-188270-195407-208918-209206-211605-230049-230916-231651&reportName=OdometerReport&EndDate=$(date -d "last-thursday" +'%Y-%m-%d')+23%3a59%3a59&StartDate=$(date -d '2 weeks ago friday' +'%Y-%m-%d')" \
-"2nd link: Quick comparison of Odometer Report values:" \
-"https://ractelematics.rac.co.uk/Report/Run/Report/?id=21&group=1437-15833&vehicles=32434-32820-100564-100565-113532-113533-113534-121872-152535-152536-152537-153363-184163-188268-188269-188270-195407-208918-209206-211605-230049-230916-231651&reportName=OdometerReport&EndDate=$(date -d "last-thursday" +'%Y-%m-%d')%2023%3A59%3A59&StartDate=$(date -d '2 weeks ago friday' +'%Y-%m-%d')"
+[ -z "$1" ] && {
+  blue='\033[0;34m'
+  reset='\033[0m'
+  printf "%s\n$blue%s$reset\n\n%s\n$blue%s$reset\n" \
+  "1st link - Download Odometer Report:" \
+  "https://ractelematics.rac.co.uk/Report/Export/CSVExport/?id=21&group=1437-15833&vehicles=32434-32820-100564-100565-113532-113533-113534-121872-152535-152536-152537-153363-184163-188268-188269-188270-195407-208918-209206-211605-230049-230916-231651&reportName=OdometerReport&EndDate=$(date -d "last-thursday" +'%Y-%m-%d')+23%3a59%3a59&StartDate=$(date -d '2 weeks ago friday' +'%Y-%m-%d')" \
+  "2nd link: Quick comparison of Odometer Report values:" \
+  "https://ractelematics.rac.co.uk/Report/Run/Report/?id=21&group=1437-15833&vehicles=32434-32820-100564-100565-113532-113533-113534-121872-152535-152536-152537-153363-184163-188268-188269-188270-195407-208918-209206-211605-230049-230916-231651&reportName=OdometerReport&EndDate=$(date -d "last-thursday" +'%Y-%m-%d')%2023%3A59%3A59&StartDate=$(date -d '2 weeks ago friday' +'%Y-%m-%d')"
+}
 
 [ -n "$1" ] && {
+  newReport="Mileage_$(date +'%m-%d-%y').xls"
+
   cut -d, -f1,2,8 "$1" | sort -t, -k2 > "$newReport"
 
   lineNo="$(wc -l < "$newReport")"
