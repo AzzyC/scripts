35eb9b3fdab584fc8368ac19a46aaec2ee2acb20
AzzyC <azmath2000@gmail.com>
2022-08-21 21:34:57 +0100

    scripts: priv: fuel: 'fuelCost' variable; Link start date to end date
    - Fuel cost changes are imminent and starting to gradually drop. As the cost is stated twice, store it in a variable for easier manipulation
    - In the case that the script is being used on a Friday, the start date will be incorrect and causing too long of a gap
       ~ Therefore, deduct correct gap from Thursday

--- priv/fuel.sh
+++ priv/fuel.sh
@@ -3,11 +3,12 @@
 [ -z "$1" ] && {
   blue='\033[0;34m'
   reset='\033[0m'
+
   printf "%s\n$blue%s$reset\n\n%s\n$blue%s$reset\n" \
   "1st link - Download Odometer Report:" \
-  "https://ractelematics.rac.co.uk/Report/Export/CSVExport/?id=21&group=1437-15833&vehicles=32434-32820-100564-100565-113532-113533-113534-121872-152535-152536-152537-153363-184163-188268-188269-188270-195407-208918-209206-211605-230049-230916-231651&reportName=OdometerReport&EndDate=$(date -d "last-thursday" +'%Y-%m-%d')+23%3a59%3a59&StartDate=$(date -d '2 weeks ago friday' +'%Y-%m-%d')" \
+  "https://ractelematics.rac.co.uk/Report/Export/CSVExport/?id=21&group=1437-15833&vehicles=32434-32820-100564-100565-113532-113533-113534-121872-152535-152536-152537-153363-184163-188268-188269-188270-195407-208918-209206-211605-230049-230916-231651&reportName=OdometerReport&EndDate=$(date -d 'last-thursday' +'%Y-%m-%d')+23%3a59%3a59&StartDate=$(date -d 'last-thursday 6 days ago' +'%Y-%m-%d')" \
   "2nd link: Quick comparison of Odometer Report values:" \
-  "https://ractelematics.rac.co.uk/Report/Run/Report/?id=21&group=1437-15833&vehicles=32434-32820-100564-100565-113532-113533-113534-121872-152535-152536-152537-153363-184163-188268-188269-188270-195407-208918-209206-211605-230049-230916-231651&reportName=OdometerReport&EndDate=$(date -d "last-thursday" +'%Y-%m-%d')%2023%3A59%3A59&StartDate=$(date -d '2 weeks ago friday' +'%Y-%m-%d')"
+  "https://ractelematics.rac.co.uk/Report/Run/Report/?id=21&group=1437-15833&vehicles=32434-32820-100564-100565-113532-113533-113534-121872-152535-152536-152537-153363-184163-188268-188269-188270-195407-208918-209206-211605-230049-230916-231651&reportName=OdometerReport&EndDate=$(date -d 'last-thursday' +'%Y-%m-%d')%2023%3A59%3A59&StartDate=$(date -d 'last-thursday 6 days ago' +'%Y-%m-%d')"
 }
 
 [ -n "$1" ] && {
@@ -17,14 +18,16 @@
 
   lineNo="$(wc -l < "$newReport")"
 
+  fuelCost='1.85'
+
   for i in $(seq 3 "$lineNo"); do
-    sed -i "${i}s/$/,=(C${i}\/35)*4.54609188*1.90/" "$newReport"
+    sed -i "${i}s/$/,=(C${i}\/35)*4.54609188*$fuelCost/" "$newReport"
   done
 
   sed -i \
   -e '2s/$/,Fuel Cost (£)/' \
   -e '3s/$/,,Miles -> Fuel Cost (£) =/' \
-  -e '4s/$/,,( Miles \/ MPG (35) ) x 4.54609188 (litres in gallon) x Cost per litre (£1.90)/' \
+  -e "4s/$/,,( Miles \/ MPG (35) ) x 4.54609188 (litres in gallon) x Cost per litre (£$fuelCost)/" \
   -e '6s/$/,,> 1 Car: Grace; Jeff; Mill; Night:/' "$newReport"
 
   grace="$(grep -in 'Grace' "$newReport" | tail -n1 | awk -F ':' '{print $1}')"
