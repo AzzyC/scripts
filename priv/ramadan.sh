9ddd9151e36bfc839455070ffb4fb6c870258ff4
AzzyC <azmath2000@gmail.com>
2023-04-03 21:57:04 +0100

    scripts: priv: ramadan: Pass argument to run script in loop
    - When Maghrib near, personally run script in loop as countdown of when can break fast

--- priv/ramadan.sh
+++ priv/ramadan.sh
@@ -1,43 +1,49 @@
 #!/bin/sh
-currentDate=".$(date +'%d%m%y')pray"
-tempFile="$HOME/$currentDate"
 
-find ~ -maxdepth 1 ! -name "${currentDate}*" -name '*pray*' -exec rm '{}' \;
+while true; do
 
-[ -e "$tempFile" ] || curl -s 'https://shahjalalmosque.org' > "$tempFile"
-[ -e "${tempFile}2" ] || grep 'Begins' -A 1 "$tempFile" | sed 'N;s/\n/ /; s/<[^>]*>/ /g; s/Begins//' > "${tempFile}2"
+  currentDate=".$(date +'%d%m%y')pray"
+  tempFile="$HOME/$currentDate"
 
-f="$(awk '{print $1}' "${tempFile}2")"
-s="$(awk '{print $2}' "${tempFile}2")"
-m="$(( $(awk -F ':[0-9]{2}' '{print $5}' "${tempFile}2" | awk '{sub(/^  0*/,"");}1') + 12 )):$(awk -F '[0-9]{2}:' '{print $6}' "${tempFile}2")"
+  find ~ -maxdepth 1 ! -name "${currentDate}*" -name '*pray*' -exec rm '{}' \;
 
-isldate="$(grep "$(date +'%d %B')" "${tempFile}" | awk '{print $7,$8,$9}')"
-days="$(printf '%s' "$isldate" | awk '{print $1}')"
+  [ -e "$tempFile" ] || curl -s 'https://shahjalalmosque.org' > "$tempFile"
+  [ -e "${tempFile}2" ] || grep 'Begins' -A 1 "$tempFile" | sed 'N;s/\n/ /; s/<[^>]*>/ /g; s/Begins//' > "${tempFile}2"
 
-print_prayertimelist () {
-  clear
-  printf '%s\n' "" "Prayer Times: $(date +'%d/%m/%Y') | $isldate
-        Fajr: ${f}
-     Sunrise: ${s}
-     Maghrib: ${m}"
-}
+  f="$(awk '{print $1}' "${tempFile}2")"
+  s="$(awk '{print $2}' "${tempFile}2")"
+  m="$(( $(awk -F ':[0-9]{2}' '{print $5}' "${tempFile}2" | awk '{sub(/^  0*/,"");}1') + 12 )):$(awk -F '[0-9]{2}:' '{print $6}' "${tempFile}2")"
 
-now="$(date +%s)"
+  isldate="$(grep "$(date +'%d %B')" "${tempFile}" | awk '{print $7,$8,$9}')"
+  days="$(printf '%s' "$isldate" | awk '{print $1}')"
+  [ -n "$days" ] || rm "${tempFile}" "${tempFile}2"
 
-[ "$now" -ge "$(date -d "${m}" +%s)" ] && {
-  print_prayertimelist
-  printf '\n\033[1;92m%s\033[0m\n' "$days fasts completed, Alhamdulillah!"
-  exit 0
-} || {
-  print_prayertimelist |
-  for time in "${f}" "${m}"; do
-    pray="$(date -d "$time" +%s)"
-    [ "$pray" -gt "$now" ] && {
-      diff="$(( pray-now ))"
-      sed "s/${time}/$(printf '\033[1;93m%s' "$time") $(printf '\033[12;94m%02dh:%02dm:%02ds\033[0m' "$(( diff / 3600 ))" "$(( ( diff % 3600 ) / 60 ))" "$(( diff % 60 ))" )/"
-      break
-    }
-  done
-  printf '\n\033[1;92m%s\033[0m\n' "$((days - 1)) fasts completed, Alhamdulillah!"
-  exit 1
-}
+  print_prayertimelist () {
+    clear
+    printf '%s\n' "" "Prayer Times: $(date +'%d/%m/%Y') | $isldate
+          Fajr: ${f}
+       Sunrise: ${s}
+       Maghrib: ${m}"
+  }
+
+  now="$(date +%s)"
+
+  [ "$now" -ge "$(date -d "${m}" +%s)" ] && {
+    print_prayertimelist
+    printf '\n\033[1;92m%s\033[0m\n' "$days fasts completed, Alhamdulillah!"
+  } || {
+    print_prayertimelist |
+    for time in "${f}" "${m}"; do
+      pray="$(date -d "$time" +%s)"
+      [ "$pray" -gt "$now" ] && {
+        diff="$(( pray-now ))"
+        sed "s/${time}/$(printf '\033[1;93m%s' "$time") $(printf '\033[12;94m%02dh:%02dm:%02ds\033[0m' "$(( diff / 3600 ))" "$(( ( diff % 3600 ) / 60 ))" "$(( diff % 60 ))" )/"
+        break
+      }
+    done
+    printf '\n\033[1;92m%s\033[0m\n' "$((days - 1)) fasts completed, Alhamdulillah!"
+  }
+
+  [ "$1" = loop ] || exit
+
+done
