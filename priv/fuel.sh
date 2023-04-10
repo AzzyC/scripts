20b0d1c6d7502a10ba6b6831d7df6e8af6e2b0c3
AzzyC <azmath2000@gmail.com>
2023-04-10 17:40:45 +0100

    scripts: priv: fuel: Update vehicles
    - Further vehicle additions and changes

--- priv/fuel.sh
+++ priv/fuel.sh
@@ -6,9 +6,9 @@
 
   printf "%s\n$blue%s$reset\n\n%s\n$blue%s$reset\n" \
   "1st link - Download Odometer Report:" \
-  "https://ractelematics.rac.co.uk/Report/Export/CSVExport/?id=21&group=1437-15833&vehicles=32434-32820-100564-100565-113532-113533-113534-121872-144559-152535-152536-152537-153363-184163-188268-188269-188270-195407-208918-209206-211605-230049-230916-231651-235968-247526&reportName=OdometerReport&EndDate=$(date -d 'last-sunday' +'%Y-%m-%d')+23%3a59%3a59&StartDate=$(date -d 'last-sunday 6 days ago' +'%Y-%m-%d')" \
+  "https://ractelematics.rac.co.uk/Report/Export/CSVExport/?id=21&group=1437-15833&vehicles=32434-32820-100564-100565-113533-121872-144559-152535-152536-152537-153363-188268-188269-188270-195407-208918-209206-211605-230049-230916-231651-235968-247526-253321-255381&&reportName=OdometerReport&EndDate=$(date -d 'last-sunday' +'%Y-%m-%d')+23%3a59%3a59&StartDate=$(date -d 'last-sunday 6 days ago' +'%Y-%m-%d')" \
   "2nd link - Quick comparison of Odometer Report values:" \
-  "https://ractelematics.rac.co.uk/Report/Run/Report/?id=21&group=1437-15833&vehicles=32434-32820-100564-100565-113532-113533-113534-121872-144559-152535-152536-152537-153363-184163-188268-188269-188270-195407-208918-209206-211605-230049-230916-231651-235968-247526&reportName=OdometerReport&EndDate=$(date -d 'last-sunday' +'%Y-%m-%d')+23%3a59%3a59&StartDate=$(date -d 'last-sunday 6 days ago' +'%Y-%m-%d')"
+  "https://ractelematics.rac.co.uk/Report/Run/Report/?id=21&group=1437-15833&vehicles=32434-32820-100564-100565-113533-121872-144559-152535-152536-152537-153363-188268-188269-188270-195407-208918-209206-211605-230049-230916-231651-235968-247526-253321-255381&&reportName=OdometerReport&EndDate=$(date -d 'last-sunday' +'%Y-%m-%d')+23%3a59%3a59&StartDate=$(date -d 'last-sunday 6 days ago' +'%Y-%m-%d')"
 }
 
 [ -n "$1" ] && {
@@ -39,13 +39,12 @@
   -e '4s/$/,,> 1 Car: Carla; Grace; Jeff; Lawrence; Mill; Night; Teresa:/' "$newReport"
 
   carla="$(grep -in 'Carla' "$newReport" | tail -n1 | awk -F ':' '{print $1}')"
+  derw="$(grep -in 'Derw' "$newReport" | tail -n1 | awk -F ':' '{print $1}')"
   grace="$(grep -in 'Grace' "$newReport" | tail -n1 | awk -F ':' '{print $1}')"
   jeff="$(grep -in 'Jeff' "$newReport" | tail -n1 | awk -F ':' '{print $1}')"
-  law="$(grep -in 'Law' "$newReport" | tail -n1 | awk -F ':' '{print $1}')"
-  mill="$(grep -in 'Mill' "$newReport" | tail -n1 | awk -F ':' '{print $1}')"
   night="$(grep -in 'Night' "$newReport" | tail -n1 | awk -F ':' '{print $1}')"
-  tere="$(grep -in 'Tere' "$newReport" | tail -n1 | awk -F ':' '{print $1}')"
-  for car in "$carla" "$grace" "$jeff" "$law" "$mill" "$night" "$tere"; do
+  spring="$(grep -in 'Spring' "$newReport" | tail -n1 | awk -F ':' '{print $1}')"
+  for car in "$carla" "$derw" "$grace" "$jeff" "$night" "$spring"; do
     sed -i "${car}s/$/,---------->,=SUM(D${car}:D$((car - 1)))/" "$newReport"
   done
 
